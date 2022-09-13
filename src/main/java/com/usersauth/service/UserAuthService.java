b8d9bb5d5085fd4bb8e5cc5ccaab8fe470762b52
Gundamaiah Annamaina <a.gundamaiah@gmail.com>
2022-09-13 11:15:23 +0530

    code for login feature added.

--- src/main/java/com/usersauth/service/UserAuthService.java
+++ src/main/java/com/usersauth/service/UserAuthService.java
@@ -1,8 +1,28 @@
 package com.usersauth.service;
 
+import com.usersauth.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserAuthService {
 
+    private static final Map<String, User> usersMap = new HashMap<>();
+
+    static {
+        buildUsersData();
+    }
+
+    private static void buildUsersData() {
+        User another = new User("another", "the loose");
+        usersMap.put("another", another);
+    }
+
     public boolean login(String username, String password) {
+        User user = usersMap.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return true;
+        }
         return false;
     }
 }
